7b54897c700255c73cdaa3019537f62f8c194624
Heyward Hagenbuch <Heyward360@gmail.com>
2023-06-08 14:29:55 -0400

    simple rest controller

---
+++ src/main/java/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,17 @@
+package springframework.msscbrewery.services;
+
+import org.springframework.stereotype.Service;
+import springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Blue Moon")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
