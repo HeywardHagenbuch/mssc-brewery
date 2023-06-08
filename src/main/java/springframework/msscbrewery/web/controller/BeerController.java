84f9b7ed7db071a46cb951542e642ac323fd11be
Heyward Hagenbuch <Heyward360@gmail.com>
2023-06-08 15:55:33 -0400

    post mapping for creating a new beer

--- src/main/java/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/springframework/msscbrewery/web/controller/BeerController.java
@@ -1,11 +1,9 @@
 package springframework.msscbrewery.web.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springframework.msscbrewery.services.BeerService;
 import springframework.msscbrewery.web.model.BeerDto;
 
@@ -25,4 +23,17 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
+
+    @PostMapping// Post - create a new beer
+    public ResponseEntity handlePost(BeerDto beerDto) {
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //TODO add hostname to url
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
 }
