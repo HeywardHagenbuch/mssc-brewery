2c85ea9c225b3bb0c04bc45d7b187942f6757183
Heyward Hagenbuch <Heyward360@gmail.com>
2023-06-09 11:20:01 -0400

    updating api version

--- src/main/java/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -1,35 +1,34 @@
-package springframework.msscbrewery.web.controller;
+package springframework.msscbrewery.web.controller.v2;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springframework.msscbrewery.services.BeerService;
-import springframework.msscbrewery.web.model.BeerDto;
+import springframework.msscbrewery.services.v2.BeerServiceV2;
+import springframework.msscbrewery.web.model.v2.BeerDtoV2;
 
 import java.util.UUID;
 
-@Deprecated
-@RequestMapping("/api/v1/beer/")
+@RequestMapping("/api/v2/beer/")
 @RestController
-public class BeerController {
+public class BeerControllerV2 {
 
-    private final BeerService beerService;
+    private final BeerServiceV2 beerServiceV2;
 
-    public BeerController(BeerService beerService) {
-        this.beerService = beerService;
+    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
+        this.beerServiceV2 = beerServiceV2;
     }
 
     @GetMapping({"/{beerId}"})
-    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
+    public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId){
 
-        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+        return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody BeerDtoV2 beerDto){
 
-        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         //todo add hostname to url
@@ -39,9 +38,9 @@ public class BeerController {
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDto){
 
-        beerService.updateBeer(beerId, beerDto);
+        beerServiceV2.updateBeer(beerId, beerDto);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
@@ -50,7 +49,7 @@ public class BeerController {
     @ResponseStatus(HttpStatus.NO_CONTENT) // this annotation is used to tell spring to not return a body
     // this is the same as return new ResponseEntity(HttpStatus.NO_CONTENT);
     public void deleteBeer(@PathVariable("beerId") UUID beerId){
-        beerService.deleteById(beerId);
+        beerServiceV2.deleteById(beerId);
     }
 
 }
---
+++ src/main/java/springframework/msscbrewery/web/model/v2/BeerStyleEnum.java
@@ -0,0 +1,11 @@
+package springframework.msscbrewery.web.model.v2;
+
+public enum BeerStyleEnum {
+    LAGER,
+    ALE,
+    STOUT,
+    PORTER,
+    IPA,
+    PALE_ALE,
+    BITTER
+}
